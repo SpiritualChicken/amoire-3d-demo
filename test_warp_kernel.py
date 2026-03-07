2757bdfd611b6336154aacd138aa34dcbfa96a74
Charlie Newlinds <cnewlinds@gmail.com>
2026-03-07 12:27:50 +1100

    Test Warp kernel on CUDA instead of CPU

--- test_warp_kernel.py
+++ test_warp_kernel.py
@@ -6,6 +6,6 @@ def test(a: wp.array(dtype=float)):
     i = wp.tid()
     a[i] = float(i)
 
-arr = wp.zeros(10, dtype=float, device="cpu")
-wp.launch(test, dim=10, inputs=[arr], device="cpu")
+arr = wp.zeros(10, dtype=float, device="cuda:0")
+wp.launch(test, dim=10, inputs=[arr], device="cuda:0")
 print("Kernel OK:", arr.numpy())
